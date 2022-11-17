f8afbb3de55f1bf5ab0d8c606d2ce7fbd45d3b31
Mikhail Andreev <114064091+misha-andreev@users.noreply.github.com>
2022-11-17 12:51:04 +0800

    Add promo parametrization in test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,14 @@
 from .pages.product_page import ProductPage
 from selenium.webdriver.common.by import By
+import  pytest
 
-link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-
-def test_guest_can_add_product_to_basket(browser):
-    page = ProductPage(browser, link)  
+@pytest.mark.parametrize('code', [*range(1,7), pytest.param(7, marks=pytest.mark.xfail), *range(8,10)])
+def test_guest_can_add_product_to_basket(browser, code):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{code}"
+    page = ProductPage(browser, link)
     page.open()
     page.click_add_to_basket_button()
     page.solve_quiz_and_get_code()
+    page.should_be_product_in_basket()
+
+# python3 pytest -v -s --tb=line /Users/mac/stepik_project/test_product_page.py
